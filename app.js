2de9b0a09ba310a4b1eb6a83d8e723f9f567c130
Nap20192 <kogay20192@gmail.com>
2025-02-10 23:21:13 +0500

    ff

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ app.get("/:customListName", async function(req, res){
 
 
 
-
-app.listen(3000, function(){
+const PORT = process.env.PORT || 3000;  
+app.listen(PORT, function(){
     console.log("Server is running on port 3000");
 });
